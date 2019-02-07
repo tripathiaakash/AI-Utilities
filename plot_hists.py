00cfce1fcfba025145f24e875cd1b950f9229225
Aakash Tripathi <saaakaaa95@gmail.com>
2019-02-07 16:26:48 +0530

    Add files via upload
    The module has a function plot_hists(df) which takes pandas dataframe as a input with all numerical columns and plots histograms of all the columns (or features)
    
    Dependency: math, seaborn, matplotlib

---
+++ plot_hists.py
@@ -0,0 +1,23 @@
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import math
+
+def plot_hists(df):
+    n_features = len(df.columns.values)
+    
+    ncol = 2
+    nrow = math.ceil(n_features/ncol)
+    figsize = (20, nrow*5)
+    
+    f, axes = plt.subplots(nrow, ncol, figsize = figsize)
+    i = 0
+    j = 0
+    n = 0
+    for colname in df.columns.values:
+        sns.distplot(df[colname], color = "blue", ax = axes[i, j])
+        n+=1
+        i = int(n/2)
+        j = int((j+1)%2)
+
+
